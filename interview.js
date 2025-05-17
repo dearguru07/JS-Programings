fa9d0d51bdac938ad48d548fa4b8754dbb24d214
dearguru07 <pangalaguruyadav101@gmail.com>
2025-05-18 00:00:31 +0530

    loops

--- interview.js
+++ interview.js
@@ -10,7 +10,6 @@
 // };
 // console.log(removeDuplicates(arr));
 
-
 // const arr1 = [1, 2, 5, 4, 5, 8, 5, 7, 5, 1, 2, 5, 8, 69];
 
 // function removeDuplicate(arr){
@@ -23,7 +22,6 @@
 //     return result
 // }
 // console.log(removeDuplicate(arr1))
-
 
 // const numbers=[1,2,5,4,87,5,2,8,2,4,8,2,1,4,5,2,1,4,5,99]
 
@@ -38,7 +36,6 @@
 // }
 // console.log(values(numbers))
 
-
 // const Factorial=(n)=>{
 //     if(n===0||n===1){
 //         return 1;
@@ -46,7 +43,6 @@
 //     return n*Factorial(n-1)
 // }
 // console.log(Factorial(5))
-
 
 // function Factorial(n){
 //     let res=1;
@@ -67,7 +63,6 @@
 // }
 // console.log(Fibnocci(10))
 
-
 // const Fib=(n)=>{
 //     let fib=[0,1]
 //     for(let i=2;i<=n;i++){
@@ -77,12 +72,33 @@
 // }
 // console.log(Fib(10))
 
-
 // let furu=[2,5,4,7,5]
 // if(furu.has(2)) true;
 
-let nums=[0,1,2,3,4,5,6,7,8,9,10]
-nums.splice(1,3,'guru')
-let res=nums.slice(-5)
-console.log(nums)
-console.log(res)
+// let nums=[0,1,2,3,4,5,6,7,8,9,10]
+// nums.splice(1,3,'guru')
+// let res=nums.slice(5)
+// console.log(nums)
+// console.log(res)
+
+// for Of for Arrys--------
+
+// let names = ["Guru", "Charan", "Mahesh", "Ram", "Venky", "Mark"];
+// for (let i of names) {
+//   console.log(i);
+// }
+
+// for In for Objects--------
+
+let namesSrtL = {
+  G: "Guru",
+  C: "Charan",
+  M: "Mahesh",
+  R: "Ram",
+  V: "Venky",
+  M: "Mark",
+};
+for (let i in namesSrtL) {
+//   console.log(namesSrtL[i]);
+  console.log(`${i} and ${namesSrtL[i]}`);
+}
