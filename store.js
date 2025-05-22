015c209c1e2789827f14e43ca7904a69ba021442
dearguru07 <pangalaguruyadav101@gmail.com>
2025-05-22 22:45:31 +0530

    don

--- store.js
+++ store.js
@@ -1,61 +1,17 @@
 
-let nonZeros=[]
-let zeros=[]
+// let nonZeros=[]
+// let zeros=[]
 
-for(let i=0;i<nums.length;i++){
-    if(nums[i]!=0){
-        nonZeros.push(nums[i])
-    }
-    else{
-        zeros.push(nums[i])
-    }
-}
-const result=[...nonZeros,zeros]
-console.log(result)
-
-
-
-let nonZeros=[]
-let zeros=[]
-
-for(let i=0;i<nums.length;i++){
-    if(nums[i]!=0){
-        nonZeros.push(nums[i])
-    }
-    else{
-        zeros.push(nums[i])
-    }
-}
-const result=[...nonZeros,zeros]
-console.log(result)
-
-let nonZeros=[]
-let zeros=[]
-
-for(let i=0;i<nums.length;i++){
-    if(nums[i]!=0){
-        nonZeros.push(nums[i])
-    }
-    else{
-        zeros.push(nums[i])
-    }
-}
-const result=[...nonZeros,zeros]
-console.log(result)
-
-let nonZeros=[]
-let zeros=[]
-
-for(let i=0;i<nums.length;i++){
-    if(nums[i]!=0){
-        nonZeros.push(nums[i])
-    }
-    else{
-        zeros.push(nums[i])
-    }
-}
-const result=[...nonZeros,zeros]
-console.log(result)
+// for(let i=0;i<nums.length;i++){
+//     if(nums[i]!=0){
+//         nonZeros.push(nums[i])
+//     }
+//     else{
+//         zeros.push(nums[i])
+//     }
+// }
+// const result=[...nonZeros,zeros]
+// console.log(result)
 
 
 // const nonZeros=nums.filter((x)=>x!=0)
